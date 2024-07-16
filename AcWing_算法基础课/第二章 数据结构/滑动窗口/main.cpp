b074a7a444e470b31e112913d2f3a7930c4d5238
Kariseven323 <2724283447@qq.com>
2024-07-16 15:09:45 +0800

    滑动窗口

---
+++ AcWing_算法基础课/第二章 数据结构/滑动窗口/main.cpp
@@ -0,0 +1,32 @@
+//https://www.acwing.com/activity/content/punch_the_clock/11/
+
+
+#include<iostream>
+using namespace std;
+const int N=1e6+10;
+int a[N],q[N];
+int main()
+{
+    cin.tie(0);
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<n;i++) cin>>a[i];
+    int hh=0,tt=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(hh<=tt&&i-k+1>q[hh]) hh++;
+        while(hh<=tt&&a[q[tt]]>a[i]) tt--;
+        q[++tt]=i;
+        if(i>=k-1) cout<<a[q[hh]]<<" ";
+    }
+    puts("");
+    hh=0,tt=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(hh<=tt&&i-k+1>q[hh]) hh++;
+        while(hh<=tt&&a[q[tt]]<a[i]) tt--;
+        q[++tt]=i;
+        if(i>=k-1) cout<<a[q[hh]]<<" ";
+    }
+    return 0;
+}
